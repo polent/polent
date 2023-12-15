bc0c5eeb7b3302567583e8695b2cea095fd45d94
Holger Hellinger <holger.hellinger@publicissapient.com>
2023-12-15 11:19:06 +0100

    release

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ const YELLOW = "\x1b[33m";
 const WHITE = "\x1b[37m";
 
 const cText = (text, color) => {
-    if(color == null){
-        color = WHITE;
-    }
-    return color + text + RESET;
-}
+  if (color == null) {
+    color = WHITE;
+  }
+  return color + text + RESET;
+};
 console.log(cText("###################################", YELLOW));
 console.log(cText("  Hello!", GREEN));
 console.log(cText("  I'm Polent, some call me Holger!", YELLOW));
-console.log(cText(`
+console.log(
+  cText(
+    `
 
   Blog: https://polente.de
+  B-Blog: https://b.polente.de
   WTF: https://hellinger.wtf
   Me : https://www.holger-hellinger.de
   GitHub: https://github.com/polent
 
-`, WHITE));
+`,
+    WHITE
+  )
+);
 console.log(cText("  Have a great life!", GREEN));
 console.log(cText("###################################", YELLOW));
